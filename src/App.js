f4f4809169eb68ea1029bf67ee4a147c12108d25
James Begin <j3begin@uwaterloo.ca>
2024-09-14 18:46:11 -0400

    dropdown and stufff

--- src/App.js
+++ src/App.js
@@ -8,19 +8,49 @@ function App() {
   const[searchQuery, setSearchQuery] = useState('');
   const[error, setError] = useState(null);
   const [data, setData] = useState(null);
+  const[dropDownOption, setDropDownOption] = useState('');
+  const[mealType, setMealType] = useState('');
 
+
+  
   const handleSearch = async (e) => {
     e.preventDefault();
     setError(null);
 
     try {
-      const userQuery = await axios.get(`http://127.0.0.1:5000/recipe?q=${searchQuery}`);
+      const userQuery = await axios.get('http://127.0.0.1:5000/recipe', {
+        params: {
+          q: searchQuery,
+          health_filter: dropDownOption,
+          mealType: mealType
+        }});
       setData(userQuery.data);
     } catch (err) {
       console.error('Error searching', err);
 
     }
   }
+
+  const dropDownHealth = (e) => {
+    setDropDownOption(e.target.value);
+  };
+  const healthOptions = [{value: 'crustacean-free', label: 'Shellfish Free'},
+                          {value: 'dairy-free', label: 'Dairy Free'},
+                          {value: 'egg-free', label: 'Egg Free'},
+                          {value: 'fish-free', label: 'Fish Free'},
+                          {value: 'gluten-free', label: 'Gluten Free'},
+                          {value: 'peanut-free', label: 'Peanut Free'},
+                          {value: 'soy-free', label: 'Soy Free'},
+                          {value: 'vegan', label: 'Vegan'},
+                          {value: 'vegetarian', label: 'Vegetarian'},
+                          {value: 'wheat-free', label: 'Wheat Free'},
+                          {value: 'kosher', label: 'Kosher'}
+  ];
+
+  const mealTypeButtons = (mealType) => {
+    setMealType(mealType);
+  }
+
 
   return (
     <div className="App">
@@ -35,6 +65,39 @@ function App() {
           <button type='submit'>Search</button>
           
         </form>
+        <label htmlFor="healthOptions">Choose an option: </label>
+        <select id="healthOptions" value={dropDownOption} onChange={dropDownHealth}>
+          <option value="">--Please choose an option--</option>
+          {healthOptions.map((healthOptions) => (
+          <option key={healthOptions.value} value={healthOptions.value}>
+          {healthOptions.label}
+          </option>
+          ))}
+          </select>
+
+        <div style={{ marginTop: '30px' }}>
+          <button
+              type="button"
+              onClick={() => mealTypeButtons('breakfast')}
+              style={{backgroundColor: mealType === 'breakfast' ? 'blue' : 'white'}}
+              >
+                Breakfast
+              </button>
+          <button
+              type="button"
+              onClick={() => mealTypeButtons('lunch')}
+              style={{backgroundColor: mealType === 'lunch' ? 'blue' : 'white'}}
+              >
+                Lunch
+              </button>
+          <button
+              type="button"
+              onClick={() => mealTypeButtons('dinner')}
+              style={{backgroundColor: mealType === 'dinner' ? 'blue' : 'white'}}
+              >
+                Dinner
+              </button>
+            </div>
         <RecipeList recipes={data}></RecipeList>
 
       </header>
