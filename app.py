6458c7a929d1da4fe13fd43634718bc161bc7914
Xuan Zheng <eagledude200@gmail.com>
2024-09-14 11:26:23 -0400

    fixed wrong app.js being claled

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app)
+
+
 @app.route("/")
 def home():
     return "Hello World!"
--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios'
 import RecipeList from './RecipeList.js';
 import './styles/App.css';
 
 
 function App() {
-  
-  
+  const [data, setData] = useState(null);
+  const [message, setMessage] = useState('');
+
+  // Fetch data from Flask API when the component mounts
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/recipe?q=chicken')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="content">
-        <p>Hello World</p>
+        {JSON.stringify(data, null, 2)}
+        <p>Not World</p>
         <RecipeList></RecipeList>
       </header>
     </div>
