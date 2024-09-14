dc173dbd634413efeee2f8336b33af81366c88e0
James Begin <103123677+James-Begin@users.noreply.github.com>
2024-09-14 10:49:42 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,49 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-//import RecipeList from './RecipeList.js';
-//import './styles/App.css';
+import axios from 'axios'
+import RecipeList from './RecipeList.js';
+import './styles/App.css';
+
 
 function App() {
   const [data, setData] = useState(null);
   const [message, setMessage] = useState('');
+
+  // Fetch data from Flask API when the component mounts
   useEffect(() => {
-    axios.get('http://127.0.0.1:5000/recipe?q=chicken')
-      .then(response => {
-        setData;
-      })
+    fetch('http://127.0.0.1:5000/recipe?q=chicken')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
-
-  const submitData = () => {
-    const data = {
-      message: 'wazzup'
-
-    };};
-    /*axios.post('http://127.0.0.1:5000/recipe', data)
-      .then(response => {
-        console.log(response.data.status);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    };*/
-
+  
   return (
     <div className="App">
-      <h1>React and Flask Integration</h1>
-
-        {/* Display data fetched from Flask */}
-        <div>
-          <h2>Data from Flask:</h2>
-          {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
-        </div>
-
-        {/* Button to send POST request to Flask */}
-        <div>
-          <button onClick={submitData}>Submit Data to Flask</button>
-          <p>{message}</p>
-        </div>
       <header className="content">
+      {JSON.stringify(data, null, 2)}
         <p>Hello World</p>
+        <RecipeList></RecipeList>
       </header>
     </div>
   );
